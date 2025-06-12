4674547327cfbb816665f85a7e390c54383058bf
Martijn <m.van.der.willigen@sintlucasedu.nl>
2025-06-12 13:34:26 +0200

    update box hitbox

--- src/ts/objects/box.ts
+++ src/ts/objects/box.ts
@@ -1,4 +1,4 @@
-import { Actor, CollisionType, DegreeOfFreedom, Keys, Vector } from "excalibur";
+import { Actor, CollisionType, Vector, } from "excalibur";
 import { Resources } from "../resources.ts";
 
 export class Box extends Actor {
@@ -10,5 +10,10 @@ export class Box extends Actor {
         });
         this.graphics.use(Resources.Box.toSprite());
         this.pos = new Vector(x, y)
+        this.collider.useBoxCollider(70, 70);
+
+        this.body.mass = 1000; 
+        this.body.friction = 0;
+        this.body.bounciness = 0.1; 
     }
 }
