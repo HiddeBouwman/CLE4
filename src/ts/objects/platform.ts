949f196548db07d36c51fcefdd92550b7b34b0f4
neotroenopawiro <302869724@student.rocmondriaan.nl>
2025-06-13 10:39:18 +0200

    platform for colors/players

--- src/ts/objects/platform.ts
+++ src/ts/objects/platform.ts
@@ -1,19 +1,32 @@
-import { Actor, CollisionType, Vector, Shape } from "excalibur";
+import { Actor, CollisionType, Vector, Shape, Color } from "excalibur";
 import { Resources } from "../resources.ts";
 
 export class Platform extends Actor {
-    constructor(x: number, y: number, width: number, height: number) {
+    playerNumber: number; // 1 of 2
+
+    constructor(x: number, y: number, width: number, height: number, playerNumber: number) {
         super({
             width: width,
             height: height,
             collisionType: CollisionType.Fixed
         });
-        this.graphics.use(Resources.Platform.toSprite());
+
+        this.playerNumber = playerNumber;
         this.pos = new Vector(x, y);
 
-        // Maak een custom collider, bijvoorbeeld 8px naar beneden en iets kleiner
-        const colliderHeight = height - 8; // bijvoorbeeld 8px minder hoog
-        const colliderOffset = new Vector(0, 4); // 4px naar beneden (pas aan naar wens)
+        // kies sprite of kleur op basis van speler
+        if (playerNumber === 1) {
+            this.graphics.use(Resources.Platform.toSprite());
+        } else {
+            // bijvoorbeeld een andere sprite of een tint
+            const sprite = Resources.Platform.toSprite();
+            sprite.tint = Color.Black; 
+            this.graphics.use(sprite);
+        }
+
+        // custom collider
+        const colliderHeight = height - 8;
+        const colliderOffset = new Vector(0, 4);
         const box = Shape.Box(width, colliderHeight, colliderOffset);
         this.collider.set(box);
     }
