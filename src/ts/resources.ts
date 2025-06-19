c734494e18cf6814b507da5c0309ac83524fbd9b
HiddeBouwman <bouwmanhidde@gmail.com>
2025-06-19 11:13:32 +0200

    - updated /fixed pressure plates - adding audio

--- src/ts/resources.ts
+++ src/ts/resources.ts
@@ -1,22 +1,54 @@
 import { FontSource, ImageFiltering, ImageSource, Loader, Resource, Sound } from "excalibur";
 
-
 // voeg hier jouw eigen resources toe
 const Resources = {
-// Audio
+
+  // Audio
   // Player audio
-  Walking: new Sound("sounds/run.mp3"),
-  Jump: new Sound("sounds/jump.wav"),
-  deathSound1: new Sound("sounds/deathsound.mp3"),
-  deathSound2: new Sound("sounds/deathsound2.mp3"),
+  Player1GetsBoosted: new Sound("sounds/player/player1GetsBoosted.mp3"),
+  Player2GetsBoosted: new Sound("sounds/player/player2GetsBoosted.mp3"),
+  PlayerDeathSound1: new Sound("sounds/player/playerDeathSound1.mp3"),
+  PlayerDeathSound2: new Sound("sounds/player/playerDeathSound2.mp3"),
+  PlayerDeathSound3: new Sound("sounds/player/playerDeathSound3.mp3"),
+  PlayerIdle1: new Sound("sounds/player/playerIdle1.mp3"),
+  PlayerIdle2: new Sound("sounds/player/playerIdle2.mp3"),
+  PlayerIdle3: new Sound("sounds/player/playerIdle3.mp3"),
+  PlayerIdle4: new Sound("sounds/player/playerIdle4.mp3"),
+  PlayerIdle5: new Sound("sounds/player/playerIdle5.mp3"),
+  PlayerIdle6: new Sound("sounds/player/playerIdle6.mp3"),
+  PlayerIdle7: new Sound("sounds/player/playerIdle7.mp3"),
+  PlayerJump: new Sound("sounds/player/playerJump.mp3"),
+  PlayerLand1: new Sound("sounds/player/playerLand1.mp3"),
+  PlayerLand2: new Sound("sounds/player/playerLand2.mp3"),
+  PlayerLandSoft1: new Sound("sounds/player/playerLandSoft1.mp3"),
+  PlayerLandSoft2: new Sound("sounds/player/playerLandSoft2.mp3"),
+  PlayerPush: new Sound("sounds/player/playerPush.mp3"),
+  PlayerRun: new Sound("sounds/player/playerRun.mp3"),
+  // Walking: new Sound("sounds/player/run.mp3"),
+  // Jump: new Sound("sounds/player/jump.wav"),
+  // deathSound1: new Sound("sounds/player/deathsound.mp3"),
+  // deathSound2: new Sound("sounds/player/deathsound2.mp3"),
 
   // UI audio
 
 
+
   // Object audio
-  buttonSound: new Sound("sounds/button.mp3"),
-  Push: new Sound("sounds/push.mp3"),
-  Teleport: new Sound("sounds/teleport.mp3"),
+  BoxMove: new Sound("sounds/objects/boxMove.mp3"),
+  BoxMove2: new Sound("sounds/objects/boxMove2.mp3"),
+  Button: new Sound("sounds/objects/button.mp3"),
+  MetalSlam: new Sound("sounds/objects/metalSlam.mp3"),
+  PlatformMoving: new Sound("sounds/objects/platformMoving.mp3"),
+  PlatformStartMoving: new Sound("sounds/objects/platformStartMoving.mp3"),
+  PlatformStopMoving: new Sound("sounds/objects/platformStopMoving.mp3"),
+  PortalEnter: new Sound("sounds/objects/portalEnter.mp3"),
+  PortalEnter2: new Sound("sounds/objects/portalEnter2.mp3"),
+  RustyMove: new Sound("sounds/objects/rustyMove.mp3"),
+  SpikeBallRoll: new Sound("sounds/objects/spikeBallRoll.mp3"),
+  Teleport: new Sound("sounds/objects/teleport.mp3"),
+  // buttonSound: new Sound("sounds/button.mp3"),
+  // Push: new Sound("sounds/push.mp3"),
+  // Teleport: new Sound("sounds/teleport.mp3"),
 
 
   // Game audio (background music etc.)
@@ -27,7 +59,6 @@ const Resources = {
 // Sprites
   // Player
   CharacterSheet: new ImageSource("images/Character.png"),
-  Fish: new ImageSource("images/fish.png"),
 
 
   // Floor
@@ -70,6 +101,7 @@ const Resources = {
 
   // Other stage objects
   Finish: new ImageSource("images/finish.png"),
+  Door: new ImageSource("images/door.png"),
 
 
   // Background
