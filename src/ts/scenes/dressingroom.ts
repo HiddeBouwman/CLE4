918ee92668cb52c7742879cfa67ac1c7f1534410
RubenRosalia <Ruben.Rosalia2002@gmail>
2025-06-20 10:49:22 +0200

    Added level button for level 3  and dressing room

---
+++ src/ts/scenes/dressingroom.ts
@@ -0,0 +1,12 @@
+import { Scene } from "excalibur";
+
+
+export class dressingRoom extends Scene {
+    constructor() {
+        super();
+    }
+
+    onActivate() {
+        console.log("Dressingroom is loaded.");
+    }
+}
