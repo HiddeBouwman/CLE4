56bbcaddb2fa7091898e38d958167c34857ef015
RubenRosalia <Ruben.Rosalia2002@gmail>
2025-06-20 11:13:36 +0200

    Updated code to be slightly more structured

--- src/ts/scenes/dressingroom.ts
+++ src/ts/scenes/dressingroom.ts
@@ -1,12 +1,35 @@
-import { Scene } from "excalibur";
-
+import { Scene, Actor, Label, Color, Font, FontUnit, TextAlign } from "excalibur";
+import { Resources } from "../resources.ts";
 
 export class dressingRoom extends Scene {
     constructor() {
         super();
+        console.log("Dressing room");
+        Resources.Menu.play();
     }
 
-    onActivate() {
-        console.log("Dressingroom is loaded.");
+    onInitialize(engine) {
+        // Add title label at the top
+        const choosePlayer_title = new Label({
+            text: "Choose player",
+            x: engine.drawWidth / 2,
+            y: 100, 
+            font: new Font({
+                size: 48,
+                unit: FontUnit.Px,
+                color: Color.Black,
+                textAlign: TextAlign.Center
+            })
+        });
+
+        //register clicks on label
+        choosePlayer_title.on('pointerup', () => {
+            engine.goToScene('choosePlayer_title');
+            // Resources.FinishMC.stop();
+        });
+
+        // Add all buttons.
+        this.add(choosePlayer_title);
+
     }
 }
